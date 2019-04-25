14b02863351d3193a140e859a4d886386fb23b34
chengyi <732345907@qq.com>
2019-04-25 18:08:34 +0800

    修改手机号码

--- application/mobile/controller/Mycenter.php
+++ application/mobile/controller/Mycenter.php
@@ -91,13 +91,16 @@ class Mycenter extends BasicMobile
     //手机 面膜显示页面
     public function safe()
     {
-        if(session('member_info')){
-            $phone = session('member_info.tel')?session('member_info.tel'):'';
-        }
-        $this->assign('phone',$phone);
-        $this->assign('title', '安全中心');
-        return $this->fetch();
+            if(session('member_info')){
+                $phone = session('member_info.tel')?session('member_info.tel'):'';
+            }
+            $this->assign('phone',$phone);
+            $this->assign('title', '安全中心');
+            return $this->fetch();
     }
+
+
+
 
     public function ques()
     {
--- application/mobile/controller/Safe.php
+++ application/mobile/controller/Safe.php
@@ -14,7 +14,8 @@ use app\mobile\controller\Login;
 class Safe extends BasicMobile
 {
 
-    public $table = 'member_info';
+    public $table = 'member_info'; //用户表
+
 
     /**
      * 忘记密码或者修改
@@ -41,7 +42,7 @@ class Safe extends BasicMobile
              //查询手机号码
             $member_info = db($this->table)->where('tel',$data['phone'])->find();
             if(empty($member_info) || $member_info==false){
-                return ['status' => 404, 'msg' => '没有查询到改号码信息!'];
+                return ['status' => 404, 'msg' => '没有查询到该号码信息!'];
             }else{
                 $res = db($this->table)->where('tel',$data['phone'])->update(['tel'=>$data['phone'],'password'=>md5($data['pwd']. 'lyxy')]);
                 if($res){
@@ -70,7 +71,51 @@ class Safe extends BasicMobile
     //更换手机号
     public function change()
     {
+        if(request()->isPost()){
+        $data['phone'] = input('post.phone');
+        $code = input('post.code');
+        $data['mid'] = session('member_info.id');
+        //检查验证码
+        if (!$code) {
+            return ['status' => 404, 'msg' => '请输入手机验证码!'];
+        } else {
+            if (time() - session('sms_code_time') > 900) {
+                session('sms_code', null);
+                session('sms_code_time', null);
+                return ['status' => 404, 'msg' => '手机验证码已失效!'];
+            }
+            if (md5($code) != session('sms_code')) {
+                return ['status' => 404, 'msg' => '手机验证码错误!'];
+            }
+        }
+        //修改手机号
+        $res = db($this->table)->where('id',$data['mid'])->update(['tel'=>$data['phone']]);
+        if($res){
+            session('member_info',null);
+            return ['status' => 200, 'msg' => '更换手机号码成功，请重新登陆!'];
+        }else{
+            return ['status' => 404, 'msg' => '更换手机号码失败!'];
+        }
+
+    }else {
         $this->assign('title', '更改手机');
         return $this->fetch();
+      }
     }
+
+
+//    //检查手机号码是否重复
+//    public function checkphone(){
+//        if(request()->isPost()){
+//            $data['tel'] = input('post.phone');
+//            $data['id'] = session('member_info.id');
+//            $res = db($this->table)->where($data)->value('tel');
+//            if($res == $data['tel']){
+//                return ['status' => 404, 'msg' => '你好，您原来也是这个号码!'];
+//            }
+//        }
+//    }
+
+
+
 }
